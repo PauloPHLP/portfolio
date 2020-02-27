bf64df74bba2eb5d6de2bad4b06a9126092fd196
Paulo Lima <paulo.phlp@outlook.com>
2020-02-27 20:28:25 -0300

    Adding correct endpoints.

--- src/providers/ApiHandlers.js
+++ src/providers/ApiHandlers.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api/v1/articles" : ""
+  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api/v1/articles" : "https://portfolio-backend.paulophlp.now.sh/api/v1/articles"
 });
 
 export async function getArticles() {
