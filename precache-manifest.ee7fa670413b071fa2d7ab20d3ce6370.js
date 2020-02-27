b9350f825719a3346c5ae82ffda889ba3ed3371d
Paulo Lima <paulo.phlp@outlook.com>
2020-02-27 20:32:56 -0300

    Updates

---
+++ precache-manifest.ee7fa670413b071fa2d7ab20d3ce6370.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c035c41c48f5f8e937f491fe511807c6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c14aef01ea999feff9e",
+    "url": "/portfolio/static/css/main.6c25683e.chunk.css"
+  },
+  {
+    "revision": "3af9a6cd89bd3ba40cce",
+    "url": "/portfolio/static/js/2.7cf78c48.chunk.js"
+  },
+  {
+    "revision": "65cb2dbd46e9277340b77ef6a7afc298",
+    "url": "/portfolio/static/js/2.7cf78c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c14aef01ea999feff9e",
+    "url": "/portfolio/static/js/main.9f4a2ba4.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "befbbadf444d6108add5d032182ac6e1",
+    "url": "/portfolio/static/media/Curriculo.befbbadf.pdf"
+  },
+  {
+    "revision": "f6f9aaf383f00c2b10c63064c5b8f469",
+    "url": "/portfolio/static/media/IoT.f6f9aaf3.png"
+  },
+  {
+    "revision": "fedbdb2bb929772cf03ec8d643d7c7ce",
+    "url": "/portfolio/static/media/Resume.fedbdb2b.pdf"
+  },
+  {
+    "revision": "faec1d361db4bb56a52458618c19e335",
+    "url": "/portfolio/static/media/arr2b.faec1d36.jpg"
+  },
+  {
+    "revision": "ec3dc271053a689c5336649ca523af42",
+    "url": "/portfolio/static/media/brazil_flag.ec3dc271.png"
+  },
+  {
+    "revision": "9c75ca2b0c277584e827c20c0a9ac4eb",
+    "url": "/portfolio/static/media/csharp.9c75ca2b.png"
+  },
+  {
+    "revision": "0abf0024aa7d86e2b1ad924d9a7603a6",
+    "url": "/portfolio/static/media/css3.0abf0024.png"
+  },
+  {
+    "revision": "dee2f02c61a4216bb248922348baf776",
+    "url": "/portfolio/static/media/facial_recognition.dee2f02c.jpg"
+  },
+  {
+    "revision": "4f3fba57e59f5ffa746f466d0beb0918",
+    "url": "/portfolio/static/media/header-background.4f3fba57.jpeg"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/portfolio/static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "637ccbf2b702b94a2cf9a3ff6cdd7dc2",
+    "url": "/portfolio/static/media/javascript.637ccbf2.png"
+  },
+  {
+    "revision": "d42672d4d185739d26257ed5c653c740",
+    "url": "/portfolio/static/media/nodejs.d42672d4.png"
+  },
+  {
+    "revision": "6af63dd25151109a1fab8ba32425d59b",
+    "url": "/portfolio/static/media/picture.6af63dd2.jpg"
+  },
+  {
+    "revision": "7eeeec832f46d5aa89ddc86c9745b2b5",
+    "url": "/portfolio/static/media/reactjs.7eeeec83.png"
+  },
+  {
+    "revision": "ff97e132da519724034302a4a92750ef",
+    "url": "/portfolio/static/media/united-states-flag.ff97e132.png"
+  },
+  {
+    "revision": "467aae41f93b9ce926f40d0648347c93",
+    "url": "/portfolio/static/media/welcome_screen_cinq.467aae41.jpg"
+  }
+]);
